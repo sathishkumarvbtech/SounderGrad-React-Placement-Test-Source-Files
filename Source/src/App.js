35ec0eb2cb59965cf2912a90381e805e7a59860c
Sathishkumar V <108221164+sathishkumarvbtech@users.noreply.github.com>
2022-07-13 15:13:56 +0530

    Add files via upload

---
+++ Source/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Slides from './Slides';
+const title = "Slideshow App";
+
+function App({slides}) {
+    return (
+        <div>
+            <h8k-navbar header={title}></h8k-navbar>
+            <div className="App">
+                <Slides slides={slides} />
+            </div>
+        </div>
+    );
+}
+
+export default App;
